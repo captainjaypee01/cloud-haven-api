ef9a4470b60db78a99ecc476ec8d9eb2ea650889
John Paul Dala <jaypeedala31@gmail.com>
2025-05-21 00:28:49 +0800

    add fillable for user provider

--- app/Models/UserProvider.php
+++ app/Models/UserProvider.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserProvider extends Model
 {
-    //
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'id',
+        'user_id',
+        'provider',
+        'provider_id',
+        'providen_token',
+        'provider_refresh_token',
+        'created_at',
+        'updated_at'
+    ];
 }
